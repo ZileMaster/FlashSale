07068972cca883c317ddfd097a47cbd3ee24264f
ZileMaster <aleksaa.zivkovic01@gmail.com>
2026-01-30 14:51:57 +0100

    Initial commit: Clean architecture and TestDrivenDev

---
+++ Domain/Entities/Event.cs
@@ -0,0 +1,23 @@
+﻿namespace Domain.Entities;
+
+public class Event
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public int AvailableTickets { get; private set; }
+
+    public Event(string name, int totalTickets)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        AvailableTickets = totalTickets;
+    }
+
+    public void ReduceStock(int quantity)
+    {
+        if(quantity < AvailableTickets)
+            throw new InvalidOperationException("Not enough tickets available.");
+        
+        AvailableTickets -= quantity;
+    }
+}
---
+++ UnitTests/EventTests.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Entities;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace UnitTests;
+
+public class EventTests
+{
+    [Fact]
+    public void ReduceStock_Should_DecreaseQuantity_When_StockIsAvailable()
+    {
+        //Arange
+        var concert = new Event("Taylor Swift", 100);
+        int quantityToBuy = 5;
+        
+        //Act
+        concert.ReduceStock(quantityToBuy); 
+        
+        //Assert
+        Assert.Equal(95, concert.AvailableTickets);
+    }
+
+    [Fact]
+    public void ReduceStock_Should_Throw_When_NotEnoughStock()
+    {
+        //Arrange
+        var concert = new Event("Taylor Swift", 5);
+        int quantityToBuy = 10;
+        
+        //Act & Assert
+        Assert.Throws<InvalidOperationException>(() => concert.ReduceStock(quantityToBuy));
+    }
+}
